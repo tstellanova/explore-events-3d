033f93b133c70f6bcb58d07df0926f26b21346ad
tstellanova <tstellanova@users.noreply.github.com>
2019-08-09 20:47:30 -0700

    Use FirstPersonStereo  camera

--- src/main.rs
+++ src/main.rs
@@ -9,57 +9,63 @@ use std::path::Path;
 use nalgebra::{Point3, Translation3};
 use kiss3d::window::Window;
 use kiss3d::light::Light;
-use kiss3d::camera::{FirstPerson};
+use kiss3d::camera::{FirstPerson, FirstPersonStereo, Camera};
 //use kiss3d::event::{Action, Key, WindowEvent};
-//use kiss3d::resource::FramebufferManager;
-use kiss3d::context::GLContext;
+use kiss3d::resource::FramebufferManager;
+//use kiss3d::context::GLContext;
+
+
+const WINDOW_LOGICAL_WIDTH: u32 = 800;
+const WINDOW_LOGICAL_HEIGHT: u32 = 600;
 
 fn threshold_diff(new_frame: &GrayImage, old_frame: &GrayImage ) -> RgbImage {
 
   let pixel_diffs:RgbImage = map_pixels(new_frame, |x, y, p| {
-    let prior_pixel:i16 = old_frame.get_pixel(x,y)[0] as i16;
-    let new_pixel:i16 = p[0] as i16;
+    let prior_pixel:u8 = old_frame.get_pixel(x,y)[0] as u8;
+    let new_pixel:u8 = p[0] as u8;
     let delta: u8 = (new_pixel.max(prior_pixel) - new_pixel.min(prior_pixel)) as u8;
-    //let green_val = new_pixel as u8;
     if delta > 15 {
       if new_pixel > prior_pixel {
-        Rgb([delta,0, 0])
-      } else if prior_pixel > new_pixel {
-        Rgb([0, 0, delta])
-      } else {
-        Rgb([255u8, 0, 255u8])
+        Rgb([255,0, 0])
+      }
+      else {
+        Rgb([0, 0, 255])
       }
     }
-    else {  Rgb([0, 0, 0]) }
+    else {
+      let green_val = new_pixel  as u8;
+      Rgb([0, green_val, 0])
+    }
   });
 
   pixel_diffs
 }
 
 
-const WINDOW_LOGICAL_WIDTH: u32 = 640;
-const WINDOW_LOGICAL_HEIGHT: u32 = 480;
-
 fn main() {
 
-  let eye = Point3::new(0.1, 1.1, 2.5);
+  let left_eye = Point3::new(-0.1, 1.1, 2.5);
   let at = Point3::origin();
-  let mut camera = FirstPerson::new(eye, at);
+  // mean male inter-pupillary distance per
+  // Military Handbook 743A and the 2012 Anthropometric Survey of US Army Personnel
+  let ipd: f32 = 64.0/1000.0;
+//  let mut camera = FirstPerson::new(left_eye, at);
+  let mut camera = FirstPersonStereo::new(left_eye, at, ipd);
   let light_pos = Point3::new(-2.0f32, 3.0f32, 2.0f32);
-  let x_translate_step = Translation3::new(0.01, 0.0, 0.0);
-
-//  let mut ctx = GLContext::new();
+  let translate_step = Translation3::new(0.01, 0.0, 0.0);
 
   let mut window = Window::new_with_size("play", WINDOW_LOGICAL_WIDTH, WINDOW_LOGICAL_HEIGHT);
-  window.height();
-  let hidpi_fact = window.hidpi_factor();
+
+//  let hidpi_fact = window.hidpi_factor();
   let win_size = window.size();
+  println!("win_size: {:?} hidpi_factor: {} ", win_size, window.hidpi_factor());
+
   //let true_width: usize = (hidpi_fact * WINDOW_STD_WIDTH as f64) as usize;
   //let true_height: usize = (hidpi_fact * WINDOW_STD_HEIGHT as f64) as usize;
-  let true_width = win_size[0] as usize;
-  let true_height = win_size[1] as usize;
+  let physical_width = win_size[0] as usize;
+  let physical_height = win_size[1] as usize;
 
-  //let offscreen_target = FramebufferManager::new_render_target(true_width, true_height);
+  let offscreen_target = FramebufferManager::new_render_target(physical_width, physical_height);
 
   window.set_light( Light::Absolute(light_pos));
 
@@ -74,22 +80,19 @@ fn main() {
   cube3.set_local_translation(Translation3::new(2.0, 0.0, -4.0));
   cube3.set_color(1.0, 1.0, 1.0);
 
-  //let mut fbm = FramebufferManager::new();
-  //fbm.select(&offscreen_target);
+  let mut fbm = FramebufferManager::new();
+  fbm.select(&offscreen_target);
 
   //throw away garbage renders when context is first opened
   for _i in 0..5 {
     window.render_with_camera(&mut camera);
   }
 
-  let win_size = window.size();
-  println!("win_size: {:?} hidpi_factor: {} ", win_size, window.hidpi_factor());
-
-
   let mut prior_frame_opt:Option<GrayImage> = None;
   for i in 0..100 {
-    //render plain cubes to offscreen targer
-//    fbm.select(&offscreen_target);
+    println!("step: {}", i);
+    //render plain cubes to offscreen target
+    fbm.select(&offscreen_target);
     if window.render_with_camera(&mut camera) {
       let snap = window.snap_image();
       //let dims = snap.dimensions();
@@ -109,17 +112,34 @@ fn main() {
         pixel_diffs.save(img_path).unwrap();
 
         //fbm.select(fbm.screen());
-
       }
       prior_frame_opt = Some(lum_img);
 
-//            let img_name = format!("./out/frame_{}.png", i);
-//            let img_path = Path::new(&img_name);
-//            img.save(img_path).unwrap();
+//      let img_name = format!("./out/frame_{}.png", i);
+//      let img_path = Path::new(&img_name);
+//      img.save(img_path).unwrap();
     }
 
-    camera.translate_mut( &x_translate_step);
+//    camera.translate_mut( &translate_step);
+
+    let old_eye = camera.eye();
+    let new_eye =   Point3::new(old_eye[0] - 0.01,
+                                old_eye[1] ,
+                                old_eye[2] );
+
+    let old_at = camera.at();
+    let new_at = Point3::new(old_at[0] - 0.01,
+                             old_at[1] ,
+                             old_at[2] );
+    camera.look_at(new_eye, new_at);
+
   }
+  
+
+  //wait for user to close
+  fbm.select(&FramebufferManager::screen());
+  while window.render_with_camera(&mut camera) {}
+
 
 //    while window.render_with_camera(&mut camera)  {
 //        for event in window.events().iter() {
